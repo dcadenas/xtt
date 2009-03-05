ee98ef44317ae2e0232e50f5835eabf1b2b73974
Daniel Cadenas <dcadenas@gmail.com>
2009-03-05 04:47:05 -0200

    Added chart legends

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,27 +2,27 @@ module ProjectsHelper
   def link_to_project(project, text = nil, url_for_options = nil)
     link_to h(text || project.name), url_for_options || project
   end
-  
+
   def period_to_sentence(filter, date_range, hours)
     plural = hours == 1 ? 'hour' : 'hours'
     filter = filter.to_sym if filter
     final = case filter
-      when :daily
-        date_range.first.strftime("%A, %b %d")
-      when :monthly
-        "for #{date_range.first.strftime("%B")}"
-      when :'bi-weekly', :weekly
-        "#{date_range.first.strftime("%B %d")}-#{date_range.last.strftime("%d")}"
-    end
+            when :daily
+              date_range.first.strftime("%A, %b %d")
+            when :monthly
+              "for #{date_range.first.strftime("%B")}"
+            when :'bi-weekly', :weekly
+              "#{date_range.first.strftime("%B %d")}-#{date_range.last.strftime("%d")}"
+            end
     %(#{plural} <span class="daterange">#{final}</span>)
   end
-  
+
   def normalized_max(data)
     logger.warn "--------------\n#{data.inspect}"
     max = data.flatten.map { |d| d.to_f }.max.to_i
     ((max * 10 ** -1).ceil.to_f / 10 **-1).to_i
   end
-  
+
   def csv(str)
     '"' + h(str).gsub('"', '""') + '"'
   end
@@ -30,27 +30,42 @@ module ProjectsHelper
   # this is a way of having a nice common interface whether you have
   # a single data set in hours  [1,2,3,3,1,15.2]
   # or multiple data sets [[1,2,3,3,1,5],[56,2,5,2,1]]
-  def fudge_data_from(hours, chart_labels)
+  def fudge_data_from(hours, chart_labels, filter)
     chart_data = []
     days = []
     total = 0
-    if hours[0].is_a?(Array) # array of arrays -- multi-chart
+    if multi_chart?(hours) # array of arrays -- multi-chart
       hours.each do |hour|
-        data = chart_data_for(chart_labels, params[:filter], hour)
-        chart_data << ((data.sum == 0 && params[:filter] == :weekly ) ? [0] : data)
+        data = chart_data_for(chart_labels, filter, hour)
+        chart_data << ((data.sum == 0 && filter == :weekly ) ? [0] : data)
       end # hours 
       days = []
       chart_data.each { |_set| _set.each_with_index { |day,i| days[i] = days[i].to_f + day.to_f }}
       total = days.flatten.sum
     else # single axis %>
       days = hours.map { |h| h[2] }
-      chart_data = chart_data_for(chart_labels, params[:filter], hours)
-      chart_data = (chart_data.sum == 0 && params[:filter] == :weekly ) ? [0] : chart_data
+      chart_data = chart_data_for(chart_labels, filter, hours)
+      chart_data = (chart_data.sum == 0 && filter == :weekly ) ? [0] : chart_data
       total = hours.sum { |h| h[2].to_f }
     end
-    return days, chart_data, total
+    return days, chart_data, total, get_legends(hours)
   end
-  
+
+  def multi_chart?(hours)
+    hours.inspect.starts_with? "[[["
+  end
+
+  def get_legends(hours)
+    names = hours.map {|h| User.find(get_user_id(h)).login }
+    names.join("|")
+  end
+
+  def get_user_id(hour)
+    first_element = hour.first
+    return first_element.first if first_element.respond_to? :first
+    return first_element
+  end
+
   def fudge_total_daily_hours(daily_hours)
     total = 0.0
     daily_hours.each do |entry|
--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -1,4 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+include StatusesHelper
+
+def create_user name
+  User.create! :login => name, :email => "#{name}@example.com", :password => "pass", :password_confirmation => "pass"
+end
+
+def a_time
+  @a_time ||= Date.new(2001,1,1).to_time
+end
+
+def week_labels
+  @week_labels ||= ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+end
 
 describe ProjectsHelper do
   {0.5 => 10, 1 => 10, 3 => 10, 10.5 => 20, 15 => 20}.each do |max, normalized|
@@ -6,4 +19,24 @@ describe ProjectsHelper do
       normalized_max([max]).should == normalized
     end
   end
+
+  it "should show the correct legend for one user" do
+    user_1 = create_user "user1"
+    hours = [[user_1.id, a_time, 1.0]]
+
+    days, chart_data, total, legend = fudge_data_from(hours, week_labels, :weekly )
+
+    legend.should == "user1"
+  end
+
+  it "should show the correct legends for many users" do
+    user_1 = create_user "user1"
+    user_2 = create_user "user2"
+    hours = [ [[user_1.id, a_time, 1.0]], [[user_2.id, a_time, 0.5]] ]
+
+    days, chart_data, total, legend = fudge_data_from(hours, week_labels, :weekly )
+
+    legend.should == "user1|user2"
+  end
 end
+
